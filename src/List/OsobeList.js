b8f1052d651aaddeaba66ccfeddfae815308eded
jvinkovic <josip.vinkovic.josip@gmail.com>
2023-03-06 20:28:05 +0100

    AddOsoba / DeleteOsoba rijesenje

--- src/List/OsobeList.js
+++ src/List/OsobeList.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import AddOsoba from './AddOsoba';
 import Osoba from './Osoba';
 
 let currentId = 4;
@@ -11,47 +12,57 @@ export default function OsobeList(){
                                         { id: 4, ime: 'Tomislava', prezime: 'Org', godine: 26}
                                     ]);
 
-    const handleChangeIme = (tekst, index) => {
+    const handleChangeIme = (tekst, id) => {
         const noviKorisnici = [...korisnici];
-        noviKorisnici[index].ime = tekst;        
+        const korisnikZaPromjenu = noviKorisnici.find((k) => k.id === id);
+        korisnikZaPromjenu.ime = tekst;
         setKorisnici(noviKorisnici);
     }
 
-    const handleChangePrezime = (tekst, index) => {
+    const handleChangePrezime = (tekst, id) => {
         const noviKorisnici = [...korisnici];
-        noviKorisnici[index].prezime = tekst;
+        const korisnikZaPromjenu = noviKorisnici.find(k => k.id === id);
+        korisnikZaPromjenu.prezime = tekst;
         setKorisnici(noviKorisnici);
     }
 
-    const handleChangeGodine = (tekst, index) => {
+    const handleChangeGodine = (tekst, id) => {
         const noviKorisnici = [...korisnici];
-        noviKorisnici[index].godine = tekst;
+        const korisnikZaPromjenu = noviKorisnici.find(k => k.id === id);
+        korisnikZaPromjenu.godine = tekst;
         setKorisnici(noviKorisnici);
     }
 
-    const handleDeleteOsoba = (index) => {
-        const newKorisnici = [];
-        
-        for(let i = 0; i < korisnici.length; i++){
-            if(i !== index) {
-                newKorisnici.push({...korisnici[i]});
-            }
-        }
-
+    const handleDeleteOsoba = (idZaBrisanje) => {
+        // nova lista korsnika je lista onih korisnika koji NISU za brisanje
+        const newKorisnici = korisnici.filter((k) => k.id !== idZaBrisanje);
         setKorisnici(newKorisnici);
+    }
+
+    const handleAddNew = (newOsoba) => {
+        // newOsoba objekt ima sve podatke, osim ID
+        currentId++;
+        const korisnikZaDodati = {...newOsoba, id: currentId};
+        
+        const newKorisniciList = [...korisnici];
+        newKorisniciList.push(korisnikZaDodati);
+        setKorisnici(newKorisniciList);
     }
 
     return (
         <div>
-            {korisnici.map((korisnik, index) => {
+            {korisnici.map((korisnik) => {
                 return (
-                    <Osoba key={korisnik.id} osoba={korisnik}
-                                        onChangeIme={(tekst) => handleChangeIme(tekst, index)}
-                                        onChangePrezime={(tekst) => handleChangePrezime(tekst, index)} 
-                                        onChangeGodine={(tekst) => handleChangeGodine(tekst, index)}
-                                        onDelete={() => handleDeleteOsoba(index)} />
+                    <Osoba key={korisnik.id} 
+                            osoba={korisnik}
+                            onChangeIme={(tekst) => handleChangeIme(tekst, korisnik.id)}
+                            onChangePrezime={(tekst) => handleChangePrezime(tekst, korisnik.id)} 
+                            onChangeGodine={(tekst) => handleChangeGodine(tekst, korisnik.id)}
+                            onDelete={() => handleDeleteOsoba(korisnik.id)} />
                 )
             })}
+
+            <AddOsoba onAddNew={handleAddNew} />
 
             {korisnici.map((korisnik) => {
                 return (
